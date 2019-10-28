b32956addc0f646b80d6f1902135691516abb2d3
adam3371 <adam.santifaller@gmail.com>
2019-10-28 18:23:58 +0100

    fist stable version of hello ESPL

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	printf("Hello World\n");
+	int  i;
+	for(i = 0; i < 100; i++)
+	{
+	
+		printf("Hello ESPL\n");
+	}
 	return 0;
 }
