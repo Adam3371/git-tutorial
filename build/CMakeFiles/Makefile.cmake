800e4592333c7e19cf67258ece846710dea886e7
adam3371 <adam.santifaller@gmail.com>
2019-10-30 15:49:50 +0100

    adding a lot of stuff

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -42,6 +42,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/foo.dir/DependInfo.cmake"
-  "CMakeFiles/ESPL_LIB.dir/DependInfo.cmake"
+  "CMakeFiles/state_machine.dir/DependInfo.cmake"
+  "CMakeFiles/STATES_LIB.dir/DependInfo.cmake"
   )
